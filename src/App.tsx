33c6a1e84cc49eb4925d9d09dd7ed05a83cd071a
Carlos Moraes <carloshrmoraes@gmail.com>
2025-02-04 14:14:11 -0300

    fix: add missing store provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { ThemeProvider } from "styled-components";
 import Home from "./components/Home";
 import { theme } from "./style/theme";
+import { Provider } from "react-redux";
+import { store } from "./context/store";
 
 function App() {
-    return (
-        <div>
-            <ThemeProvider theme={theme}>
-                <Home />
-            </ThemeProvider>
-        </div>
-    );
+  return (
+    <div>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <Home />
+        </Provider>
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
