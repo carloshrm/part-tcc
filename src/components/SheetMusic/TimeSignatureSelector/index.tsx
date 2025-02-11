675cb6d5c48355df80efd8adc9118cdd404646e3
Carlos Moraes <carloshrmoraes@gmail.com>
2025-02-11 13:09:40 -0300

    refactor: change TimeSignature to work based on dynamic settings

--- src/components/SheetMusic/TimeSignatureSelector/index.tsx
+++ src/components/SheetMusic/TimeSignatureSelector/index.tsx
@@ -2,43 +2,52 @@ import { useAppDispatch, useAppSelector } from "@/context/hooks";
 import * as S from "./styles";
 import { getTimeSig, setTimeSig } from "@/context/MusicData/musicDataSlice";
 
+export const validTimeSignatureOptions = {
+  beats: [3, 4, 5, 6],
+  values: [4, 8, 12],
+};
+
 function TimeSignatureSelector() {
   const dispatch = useAppDispatch();
   const currentTimeSignature = useAppSelector(getTimeSig);
 
-  const handleTimeSignatureChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    if (e.target.name === "time-beats") {
-      dispatch(
-        setTimeSig({
-          beats: parseInt(e.target.value),
-          value: currentTimeSignature.value,
-        }),
-      );
-    } else if (e.target.name === "time-value") {
-      dispatch(
-        setTimeSig({
-          beats: currentTimeSignature.beats,
-          value: parseInt(e.target.value),
-        }),
-      );
-    }
+  const handleBeatChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    dispatch(
+      setTimeSig({
+        beats: parseInt(e.target.value),
+        value: currentTimeSignature.value,
+      }),
+    );
+  };
+
+  const handleValueChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    dispatch(
+      setTimeSig({
+        beats: currentTimeSignature.beats,
+        value: parseInt(e.target.value),
+      }),
+    );
   };
 
   return (
     <S.Container>
-      <select name="time-beats" onChange={(e) => console.log(e.target.value)}>
-        {[3, 4, 5, 6].map((val) => {
+      <select name="time-beats" onChange={(e) => handleBeatChange(e)} value={currentTimeSignature.beats}>
+        {validTimeSignatureOptions.beats.map((val) => {
           return (
-            <option value={val} key={val} selected={val === currentTimeSignature.beats}>
+            <option value={val} key={val}>
               {val}
             </option>
           );
         })}
       </select>
-      <select name="time-value">
-        <option value="4">4</option>
-        <option value="8">8</option>
-        <option value="12">12</option>
+      <select name="time-value" onChange={(e) => handleValueChange(e)} value={currentTimeSignature.value}>
+        {validTimeSignatureOptions.values.map((val) => {
+          return (
+            <option value={val} key={val}>
+              {val}
+            </option>
+          );
+        })}
       </select>
     </S.Container>
   );
