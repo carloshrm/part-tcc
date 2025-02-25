1d33902e7e07a3d445bb4c86956160df5cb1b9a4
Carlos Moraes <carloshrmoraes@gmail.com>
2025-02-25 20:34:24 -0300

    feat: add rests constant and placeholder notes

--- src/context/MusicData/types.ts
+++ src/context/MusicData/types.ts
@@ -17,15 +17,20 @@ export interface TimeSignature {
 export interface MusicDataState {
   clef: Clef;
   timeSignature: TimeSignature;
-  measures: Note[][];
+  notes: Note[];
 }
 
 export const RESTS: { [key: string]: Note } = {
   WHOLE: { keys: ["b/4"], duration: "1r", voice: 1 },
+  1: { keys: ["b/4"], duration: "1r", voice: 1 },
   HALF: { keys: ["b/4"], duration: "2r", voice: 1 },
+  2: { keys: ["b/4"], duration: "2r", voice: 1 },
   QUARTER: { keys: ["b/4"], duration: "4r", voice: 1 },
+  4: { keys: ["b/4"], duration: "4r", voice: 1 },
   EIGHTH: { keys: ["b/4"], duration: "8r", voice: 1 },
+  8: { keys: ["b/4"], duration: "8r", voice: 1 },
   SIXTEENTH: { keys: ["b/4"], duration: "16r", voice: 1 },
+  16: { keys: ["b/4"], duration: "16r", voice: 1 },
 };
 
 export const defaultMusicData: MusicDataState = {
@@ -34,5 +39,18 @@ export const defaultMusicData: MusicDataState = {
     beats: 4,
     value: 4,
   },
-  measures: [Array(4).fill(RESTS.QUARTER), Array(2).fill(RESTS.HALF)],
+  notes: [
+    ...Array(2).fill(RESTS.HALF),
+    ...Array(4).fill(RESTS.QUARTER),
+    ...Array(4).fill(RESTS.QUARTER),
+    ...Array(8).fill({ keys: ["b/4"], duration: "8", voice: 1 }),
+    ...Array(4).fill(RESTS.QUARTER),
+    ...Array(2).fill(RESTS.HALF),
+    ...Array(2).fill(RESTS.HALF),
+  ],
+};
+
+export const validTimeSignatureOptions = {
+  beats: [3, 4, 5, 6],
+  values: [4, 6, 8, 12, 16],
 };
