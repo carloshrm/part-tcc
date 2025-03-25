514a6f0cf1bb9a529c2146ce1fe41cfa7dbd04c5
Carlos Moraes <carloshrmoraes@gmail.com>
2025-03-25 11:14:59 -0300

    feat: add mapNotesToMeasures util function

--- src/utils/useUtils.ts
+++ src/utils/useUtils.ts
@@ -1,3 +1,4 @@
+import { RESTS } from "@/context/MusicData/constants";
 import { Note, TimeSignature } from "@/context/MusicData/types";
 import { StaveNote } from "vexflow";
 
@@ -9,13 +10,35 @@ function UseUtils() {
   const mapNotesToVexflow = (notes: Note[] | undefined) => {
     if (!notes) return [];
 
-    return notes.map((note) => {
-      return new StaveNote({
+    return notes.map((note, i) => {
+      const newNote = new StaveNote({
         keys: note.keys,
         duration: note.duration,
       });
+      newNote.setAttribute("note-id", i.toString());
+      return newNote;
     });
   };
+
+  const mapNotesToMeasures = (notes: Note[], timeSignature: TimeSignature) => {
+    const measures: Note[][] = [];
+
+    let currentMeasure: Note[] = [];
+    let currentMeasureValue = 0;
+    notes.forEach((note) => {
+      const noteValue = parseInt(note.duration);
+      currentMeasureValue += timeSignature.value / noteValue;
+      currentMeasure.push(note);
+
+      if (currentMeasureValue === timeSignature.value) {
+        measures.push(currentMeasure);
+        currentMeasure = [];
+        currentMeasureValue = 0;
+      }
+    });
+    return measures;
+  };
+
   const makeEmptyMeasure = (timeSignature: TimeSignature, duration: number) => {
     const notesPerBeat = duration / timeSignature.value;
     return Array(timeSignature.beats * notesPerBeat).fill(RESTS[duration]);
@@ -24,6 +47,7 @@ function UseUtils() {
   return {
     timeSignatureToString,
     mapNotesToVexflow,
+    mapNotesToMeasures,
     makeEmptyMeasure,
   };
 }
