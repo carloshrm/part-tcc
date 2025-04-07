97c22b8d56da79164f8c47dbcedf93f86a4bca2a
Carlos Moraes <carloshrmoraes@gmail.com>
2025-04-06 21:19:05 -0300

    refactor: change to ant.design components

--- src/components/SheetMusic/TimeSignatureSelector/index.tsx
+++ src/components/SheetMusic/TimeSignatureSelector/index.tsx
@@ -2,49 +2,44 @@ import { useAppDispatch, useAppSelector } from "@/context/hooks";
 import * as S from "./styles";
 import { getTimeSig, setTimeSig } from "@/context/MusicData/musicDataSlice";
 import { validTimeSignatureOptions } from "@/context/MusicData/constants";
+import { Select, Typography } from "antd";
 
 function TimeSignatureSelector() {
   const dispatch = useAppDispatch();
   const currentTimeSignature = useAppSelector(getTimeSig);
 
-  const handleBeatChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleBeatChange = (e: number) => {
     dispatch(
       setTimeSig({
-        beats: parseInt(e.target.value),
+        beats: e,
         value: currentTimeSignature.value,
       }),
     );
   };
 
-  const handleValueChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleValueChange = (e: number) => {
     dispatch(
       setTimeSig({
         beats: currentTimeSignature.beats,
-        value: parseInt(e.target.value),
+        value: e,
       }),
     );
   };
 
   return (
     <S.Container>
-      <select name="time-beats" onChange={(e) => handleBeatChange(e)} value={currentTimeSignature.beats}>
-        {validTimeSignatureOptions.beats.map((val) => {
-          return (
-            <option value={val} key={val}>
-              {val}
-            </option>
-          );
-        })}
-      </select>
-      <select name="time-value" onChange={(e) => handleValueChange(e)} value={currentTimeSignature.value}>
-        {validTimeSignatureOptions.values.map((val) => {
-          return (
-            <option value={val} key={val}>
-              {val}
-            </option>
-          );
-        })}
-      </select>
+      <Typography.Title level={4}>Fórmula de compasso: </Typography.Title>
+      <Select
+        defaultValue={currentTimeSignature.beats}
+        options={validTimeSignatureOptions.beats.map((b) => ({ value: b, label: b }))}
+        onChange={handleBeatChange}
+      />
+      <Typography.Text> / </Typography.Text>
+      <Select
+        defaultValue={currentTimeSignature.value}
+        options={validTimeSignatureOptions.values.map((v) => ({ value: v, label: v }))}
+        onChange={handleValueChange}
+      />
     </S.Container>
   );
 }
