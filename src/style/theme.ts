7c3d5d3108e0d25cdd39568876731531c74ae9be
Carlos Moraes <carloshrmoraes@gmail.com>
2025-04-06 21:20:08 -0300

    chore: add placeholder colors to theme

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -2,5 +2,21 @@ export const theme = {
   colors: {
     main: "black",
     background: "lightgray",
+    white: "white",
+    darkBackground: "darkgray",
+    lightBackground: "#f5f5f5",
+    highlightBackground: "darkblue",
+    highlightText: "white",
+    shadowColor: "#888888",
+    warningColor: "#bb5555",
+    noteHighlight: "#2275d3",
+    noteHover: "#6bceec",
+  },
+  borderRadius: {
+    defaultRadius: "20rem",
+    smallRadius: "1rem",
+  },
+  border: {
+    defaultBorder: "1px solid black",
   },
 };
