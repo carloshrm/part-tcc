20c3e833c6e0c83bac7f94b6bca2990a70c3440b
Carlos Moraes <carloshrmoraes@gmail.com>
2025-04-17 14:38:23 -0300

    feat: add bpm support to time signature controls

--- src/components/SheetMusic/TimeSignatureSelector/index.tsx
+++ src/components/SheetMusic/TimeSignatureSelector/index.tsx
@@ -1,12 +1,15 @@
 import { useAppDispatch, useAppSelector } from "@/context/hooks";
-import * as S from "./styles";
+// import * as S from "./styles";
 import { getTimeSig, setTimeSig } from "@/context/MusicData/musicDataSlice";
 import { validTimeSignatureOptions } from "@/context/MusicData/constants";
-import { Select, Typography } from "antd";
+import { InputNumber, Select, Typography } from "antd";
+import ControlContainer from "@/components/ControlContainer";
+import { getAllPlayerSettings, setBpm } from "@/context/Player/playerSettingsSlice";
 
 function TimeSignatureSelector() {
-  const dispatch = useAppDispatch();
   const currentTimeSignature = useAppSelector(getTimeSig);
+  const { bpm } = useAppSelector(getAllPlayerSettings);
+  const dispatch = useAppDispatch();
 
   const handleBeatChange = (e: number) => {
     dispatch(
@@ -27,7 +30,7 @@ function TimeSignatureSelector() {
   };
 
   return (
-    <S.Container>
+    <ControlContainer name="Tempo" defaultCollapsed={true}>
       <Typography.Title level={4}>Fórmula de compasso: </Typography.Title>
       <Select
         defaultValue={currentTimeSignature.beats}
@@ -40,7 +43,19 @@ function TimeSignatureSelector() {
         options={validTimeSignatureOptions.values.map((v) => ({ value: v, label: v }))}
         onChange={handleValueChange}
       />
-    </S.Container>
+      <Typography.Title level={4}>Andamento: </Typography.Title>
+      <InputNumber
+        size="small"
+        min={30}
+        max={260}
+        defaultValue={bpm}
+        onChange={(value) => {
+          if (value !== null) {
+            dispatch(setBpm(value));
+          }
+        }}
+      />
+    </ControlContainer>
   );
 }
 
