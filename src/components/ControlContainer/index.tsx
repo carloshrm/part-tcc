e01d4c082542384799432a0cb24e983ec99b6640
Carlos Moraes <carloshrmoraes@gmail.com>
2025-04-17 14:32:38 -0300

    fix: adjust styling and add prop to enable or disable collapsing the control container

--- src/components/ControlContainer/index.tsx
+++ src/components/ControlContainer/index.tsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import * as S from "./styles";
+import { CaretUpOutlined, CaretDownOutlined } from "@ant-design/icons";
 
 export interface ControlContainerProps {
   children: React.ReactNode;
   name: string;
   scroll?: boolean;
+  defaultCollapsed?: boolean;
 }
 
-function ControlContainer({ children, name, scroll = false }: ControlContainerProps) {
+function ControlContainer({ children, name, scroll = false, defaultCollapsed = false }: ControlContainerProps) {
+  const [collapsed, setCollapsed] = useState(defaultCollapsed);
+
   return (
-    <S.ControlFieldset $scroll={scroll}>
-      <legend>{name}</legend>
-      {children}
+    <S.ControlFieldset $scroll={scroll} $collapsed={collapsed}>
+      <legend onClick={() => setCollapsed(!collapsed)}>
+        {collapsed ? <CaretDownOutlined /> : <CaretUpOutlined />}
+        {name}
+      </legend>
+      {collapsed ? <></> : children}
     </S.ControlFieldset>
   );
 }
--- src/components/ControlContainer/styles.ts
+++ src/components/ControlContainer/styles.ts
@@ -1,17 +1,43 @@
 import styled from "styled-components";
 
-export const ControlFieldset = styled.fieldset<{ $scroll: boolean }>`
+export const ControlFieldset = styled.fieldset<{ $scroll: boolean; $collapsed: boolean }>`
   margin: 1rem;
-  padding: 1rem;
+  padding: ${({ $collapsed }) => ($collapsed ? "0 1rem" : "1rem")};
 
-  overflow-y: ${({ $scroll }) => ($scroll ? "scroll" : "hidden")};
-  height: ${({ $scroll }) => ($scroll ? "30vh" : "auto")};
+  width: ${({ $collapsed }) => ($collapsed ? "10rem" : "28rem")};
+
+  overflow: ${({ $scroll, $collapsed }) => (!$scroll || $collapsed ? "hidden" : "scroll")};
+  height: ${({ $scroll, $collapsed }) => {
+    if ($collapsed) {
+      return "0";
+    } else {
+      return $scroll ? "30vh" : "auto";
+    }
+  }};
+
+  transition:
+    height 0.1s ease-in-out,
+    width 0.2s ease-out,
+    padding 0.1s ease-out,
+    background-color 0.1s ease-out,
+    border-radius 0.1s ease-out;
+
+  background-color: ${({ theme, $collapsed }) => ($collapsed ? theme.colors.highlightBackground : "")};
+
+  border: 1px solid ${({ theme }) => theme.colors.highlightBackground};
+  border-radius: ${({ theme, $collapsed }) => ($collapsed ? theme.borderRadius.defaultRadius : "")};
   legend {
     font-weight: bold;
     padding: 0.4rem 1rem;
-    border: 1px solid gray;
     border-radius: ${({ theme }) => theme.borderRadius.defaultRadius};
     background-color: ${({ theme }) => theme.colors.highlightBackground};
     color: ${({ theme }) => theme.colors.highlightText};
+    transform: ${({ $collapsed }) => ($collapsed ? "translateY(20%)" : "")};
+    transition: all 0.3s ease-in-out;
+    cursor: pointer;
+    border: 1px solid ${({ theme }) => theme.colors.highlightBackground};
+    &:hover {
+      border: 2px solid ${({ theme }) => theme.colors.highlightText};
+    }
   }
 `;
