cdaae4b3262e6bb464e972672f4355f7cd809cf7
Carlos Moraes <carloshrmoraes@gmail.com>
2025-04-22 08:42:01 -0300

    fix: use symbol hook on note card

--- src/components/SheetMusic/MeasureDisplay/components/NoteCard/index.tsx
+++ src/components/SheetMusic/MeasureDisplay/components/NoteCard/index.tsx
@@ -1,7 +1,7 @@
 import { useAppDispatch, useAppSelector } from "@/context/hooks";
-import { FIGURES } from "@/context/MusicData/constants";
 import { getHoverNote, getSelectedNoteIndex, setHoverNote, setSelectedNote } from "@/context/MusicData/musicDataSlice";
 import { Note } from "@/context/MusicData/types";
+import UseMusicSymbol, { AvailableSymbols, CheckSymbol } from "@/utils/useMusicSymbol";
 import * as S from "./styles";
 
 export interface NoteCardProps {
@@ -13,6 +13,14 @@ function NoteCard({ index, note }: NoteCardProps) {
   const selectedNote = useAppSelector(getSelectedNoteIndex);
   const hoverNote = useAppSelector(getHoverNote);
   const dispatch = useAppDispatch();
+
+  const getSymbol = (duration: string) => {
+    if (CheckSymbol(duration.replace("r", ""))) {
+      return UseMusicSymbol(duration as AvailableSymbols, "SMALL");
+    }
+    return UseMusicSymbol("4");
+  };
+
   return (
     <S.CardContainer
       className={`index-${index}`}
@@ -21,7 +29,7 @@ function NoteCard({ index, note }: NoteCardProps) {
       onClick={() => dispatch(setSelectedNote(index))}
       onMouseEnter={() => dispatch(setHoverNote(index))}
     >
-      <span>{FIGURES[parseInt(note.duration)]}</span>
+      <span>{getSymbol(parseInt(note.duration).toString())}</span>
     </S.CardContainer>
   );
 }
