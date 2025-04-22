f004f52f5efb0cabab4fce4b52fc6da3915a0a3a
Carlos Moraes <carloshrmoraes@gmail.com>
2025-04-22 08:12:15 -0300

    feat: implement persistance logic for the redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import { ConfigProvider } from "antd";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import { ThemeProvider } from "styled-components";
 import "./App.css";
 import "./assets/fonts/Bravura.css";
 import Home from "./components/Home";
-import { store } from "./context/store";
+import { persistor, store } from "./context/store";
 import { theme } from "./style/theme";
 
 function App() {
   return (
-    <div>
+    <>
       <ThemeProvider theme={theme}>
         <ConfigProvider theme={{ token: { colorPrimary: theme.colors.primary } }}>
           <Provider store={store}>
-            <Home />
+            <PersistGate loading={null} persistor={persistor}>
+              <Home />
+            </PersistGate>
           </Provider>
         </ConfigProvider>
       </ThemeProvider>
-    </div>
+    </>
   );
 }
 
--- src/context/store.ts
+++ src/context/store.ts
@@ -1,13 +1,39 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { PERSIST, persistReducer, persistStore, REHYDRATE } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import musicDataReducer from "./MusicData/musicDataSlice";
 import playerSettingsReducer from "./Player/playerSettingsSlice";
 
+const persistedMusicDataReducer = persistReducer(
+  {
+    key: "musicdata",
+    storage,
+  },
+  musicDataReducer,
+);
+
+const persistedPlayerSettingsReducer = persistReducer(
+  {
+    key: "playersettings",
+    storage,
+  },
+  playerSettingsReducer,
+);
+
 export const store = configureStore({
   reducer: {
-    musicData: musicDataReducer,
-    playerSettings: playerSettingsReducer,
+    musicData: persistedMusicDataReducer,
+    playerSettings: persistedPlayerSettingsReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [PERSIST, REHYDRATE],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
