31e55670d3621aed5f3b78e49a6753d560640463
Carlos Moraes <carloshrmoraes@gmail.com>
2025-04-22 08:39:48 -0300

    fix: change styling on player buttons and use symbol hook

--- src/components/SheetMusic/MusicPlayer/index.tsx
+++ src/components/SheetMusic/MusicPlayer/index.tsx
@@ -1,12 +1,13 @@
 import ControlContainer from "@/components/ControlContainer";
 import { useAppDispatch } from "@/context/hooks";
 import { setVolume, toggleMute } from "@/context/Player/playerSettingsSlice";
+import UseMusicSymbol from "@/utils/useMusicSymbol";
 import UsePlayer from "@/utils/usePlayer";
-import { Slider, Tooltip } from "antd";
+import { Slider, Tooltip, Typography } from "antd";
 import * as S from "./styles";
 
 function MusicPlayer() {
-  const { isMuted, playMusic, stopPlayer, isPlaying, volume } = UsePlayer();
+  const { isMuted, playMusic, stopPlayer, isPlaying, volume, playNote } = UsePlayer();
   const dispatch = useAppDispatch();
 
   const handleMute = () => {
@@ -15,18 +16,30 @@ function MusicPlayer() {
 
   return (
     <ControlContainer name="Player" defaultCollapsed={true}>
-      <Tooltip title="Liga ou Desliga o som">
-        <S.MuteButton $isMuted={isMuted} onClick={handleMute}>
-          {isMuted ? "\ueb26" : "\ueb27"}
-        </S.MuteButton>
-      </Tooltip>
-      <S.PlayButton onClick={() => playMusic()} $isPlaying={isPlaying}>
-        {"\ueb1c"}
-      </S.PlayButton>
-      <S.MusicControlButton onClick={() => stopPlayer()}>{"\ueb1d"}</S.MusicControlButton>
-      <Tooltip title="Volume">
-        <Slider defaultValue={volume} min={-50} max={-5} onChange={(value) => dispatch(setVolume(value))} />
-      </Tooltip>
+      <S.ButtonContainer>
+        <S.ControlButton onClick={() => playMusic()} $isPlaying={isPlaying}>
+          Tocar {UseMusicSymbol("PLAY")}
+        </S.ControlButton>
+        <S.ControlButton onClick={() => stopPlayer()}>Parar {UseMusicSymbol("STOP")}</S.ControlButton>
+        <S.ControlButton type="default" onClick={() => playNote()}>
+          Ouvir
+        </S.ControlButton>
+      </S.ButtonContainer>
+
+      <Typography.Title level={4}>Volume</Typography.Title>
+
+      <S.VolumeContainer>
+        <Tooltip title="Liga ou desliga o som">
+          <S.MuteButton $isMuted={isMuted} onClick={handleMute}>
+            {isMuted ? UseMusicSymbol("SPEAKER_CLOSED", "SMALL") : UseMusicSymbol("SPEAKER_OPEN", "SMALL")}
+          </S.MuteButton>
+        </Tooltip>
+        <S.SliderContainer>
+          <Tooltip title="Volume">
+            <Slider defaultValue={volume} min={-50} max={-5} onChange={(value) => dispatch(setVolume(value))} />
+          </Tooltip>
+        </S.SliderContainer>
+      </S.VolumeContainer>
     </ControlContainer>
   );
 }
--- src/components/SheetMusic/MusicPlayer/styles.ts
+++ src/components/SheetMusic/MusicPlayer/styles.ts
@@ -1,23 +1,35 @@
 import { Button } from "antd";
 import styled from "styled-components";
 
-export const MusicControlButton = styled(Button).attrs({
+export const ControlButton = styled(Button).attrs({
   type: "default",
-})`
-  font-family: "Bravura", sans-serif !important;
-  font-size: 1.8rem;
-  padding-top: 1rem;
-`;
-
-export const PlayButton = styled(MusicControlButton)<{ $isPlaying: boolean }>`
+})<{ $isPlaying?: boolean }>`
   background-color: ${({ $isPlaying, theme }) => ($isPlaying ? theme.colors.secondary : "")};
+  &:hover {
+    background: ${({ $isPlaying, theme }) => ($isPlaying ? theme.colors.secondary : "")} !important;
+  }
 `;
 
 export const MuteButton = styled(Button).attrs({
   type: "default",
 })<{ $isMuted: boolean }>`
-  font-family: "Bravura", sans-serif !important;
-  padding-top: 1rem;
-  color: ${({ theme }) => theme.colors.highlightText};
   background-color: ${({ $isMuted, theme }) => ($isMuted ? theme.colors.warning : theme.colors.highlightBackground)};
+  span {
+    color: ${({ theme }) => theme.colors.highlightText};
+  }
+`;
+
+export const VolumeContainer = styled.div`
+  display: flex;
+  margin: 1rem 0;
+`;
+
+export const SliderContainer = styled.div`
+  width: 100%;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+  margin: 1rem 0;
 `;
