62502a581a7064ed6bd4e5dfebd650b8e9b2b92a
Carlos Moraes <carloshrmoraes@gmail.com>
2025-05-01 21:11:52 -0300

    feat: add modifier support for hotkey hook

--- src/utils/useHotkey.ts
+++ src/utils/useHotkey.ts
@@ -1,12 +1,26 @@
 import { useCallback, useEffect } from "react";
 
-function UseHotkey(key: string, callback: () => void) {
+interface HotkeyOptions {
+  withShift?: boolean;
+  withCtrl?: boolean;
+  withAlt?: boolean;
+}
+
+function UseHotkey(
+  key: string,
+  callback: () => void,
+  { withShift = false, withAlt = false, withCtrl = false }: HotkeyOptions = {},
+) {
   const callbackMemo = useCallback(callback, [callback]);
 
   useEffect(() => {
     const keyDownHandler = (e: KeyboardEvent) => {
+      e.preventDefault();
       if (e.key.toLowerCase() === key.toLowerCase()) {
-        e.preventDefault();
+        if ((withShift && !e.shiftKey) || (withAlt && !e.altKey) || (withCtrl && !e.ctrlKey)) {
+          return;
+        }
+
         callbackMemo();
       }
     };
